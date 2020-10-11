5cb516d9e1db0a5e20ea95bf098dcca5b64a1a79
JossinAntony <jossinde@gmail.com>
2020-10-11 16:18:43 +0200

    datamodel.dart created.

---
+++ lib/datamodel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Book{
+  String title, author;
+  double price;
+
+  Book({this.title, this.author, this.price});
+}
+
+class BookModel extends ChangeNotifier{
+  List<Book> booklist = [];
+
+  addBookToModel(String title, String author, double price){
+    booklist.add(Book(title: title, author: author,price: price));
+    notifyListeners();
+  }
+}
